34d656084c41bef5812f7b29792ef466c91b412d
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-09-24 21:15:27 +0200

    Remove unused code

--- lib/cometoid_web/live/text_live/sidebar_component.ex
+++
@@ -1,5 +0,0 @@
-defmodule CometoidWeb.TextLive.SidebarComponent do
-  # If you generated an app with mix phx.new --live,
-  # the line below would be: use MyAppWeb, :live_component
-  use Phoenix.LiveComponent
-end
