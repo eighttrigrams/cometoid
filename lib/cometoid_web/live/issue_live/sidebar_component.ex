418c94dd42993e7d16e4930d0ca73bbc31cbccab
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-11-02 20:52:40 +0100

    Handle html conversion error

--- lib/cometoid_web/live/issue_live/sidebar_component.ex
+++ lib/cometoid_web/live/issue_live/sidebar_component.ex
@@ -3,9 +3,10 @@ defmodule CometoidWeb.IssueLive.SidebarComponent do
   # the line below would be: use MyAppWeb, :live_component
   use Phoenix.LiveComponent
 
-  def convert what do
-    {:ok, html, _} = Earmark.as_html(what)
-    html
-    # String.split(what, ~r{(\r\n|\r|\n)})
+  def convert raw do
+    case Earmark.as_html raw do
+      {:ok, html, _} -> html
+      _ -> raw
+    end
   end
 end
