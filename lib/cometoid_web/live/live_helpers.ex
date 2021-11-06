59ab31fda2d6085f026ea4cfa907ba209d7efbda
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-11-07 00:55:26 +0100

    Remove apidoc

--- lib/cometoid_web/live/live_helpers.ex
+++ lib/cometoid_web/live/live_helpers.ex
@@ -1,20 +1,6 @@
 defmodule CometoidWeb.LiveHelpers do
   import Phoenix.LiveView.Helpers
 
-  @doc """
-  Renders a component inside the `CometoidWeb.ModalComponent` component.
-
-  The rendered modal receives a `:return_to` option to properly update
-  the URL when the modal is closed.
-
-  ## Examples
-
-      <%= live_modal @socket, CometoidWeb.IssueLive.FormComponent,
-        id: @issue.id || :new,
-        action: @live_action,
-        issue: @issue,
-        return_to: Routes.issue_index_path(@socket, :index) %>
-  """
   def live_modal(socket, component, opts) do
     modal_opts = [id: :modal, component: component, opts: opts]
     live_component(socket, CometoidWeb.ModalComponent, modal_opts)
