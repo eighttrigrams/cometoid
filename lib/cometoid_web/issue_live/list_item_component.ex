fa7d1c4b629ee06795fd39c0003a443af40e3cb0
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-11-14 19:49:23 +0100

    Don't show delete button under certain conditions

--- lib/cometoid_web/issue_live/list_item_component.ex
+++ lib/cometoid_web/issue_live/list_item_component.ex
@@ -3,4 +3,9 @@ defmodule CometoidWeb.IssueLive.ListItemComponent do
   # the line below would be:
   use CometoidWeb, :live_component
   # use Phoenix.LiveComponent
+
+  def should_show_delete_button state, issue do
+    is_nil(state.selected_context)
+    or length(issue.contexts) == 1
+  end
 end
