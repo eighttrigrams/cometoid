c7de9576434967106e3ee982876cb6d6a8219cc4
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2022-01-03 19:50:03 +0000

    Extract function

--- lib/cometoid_web/issue_live/list_item_component.ex
+++ lib/cometoid_web/issue_live/list_item_component.ex
@@ -5,12 +5,20 @@ defmodule CometoidWeb.IssueLive.ListItemComponent do
   # use Phoenix.LiveComponent
 
   def should_show_delete_button state, issue do
-    is_nil(state.selected_context)
-    or length(issue.contexts) == 1
+    (is_nil state.selected_context)
+    or (length issue.contexts) == 1
   end
 
   def should_show_unlink_button state, issue do
-    not is_nil(state.selected_context)
-    and length(issue.contexts) > 1
+    not (is_nil state.selected_context)
+    and (length issue.contexts) > 1
+  end
+
+  def contexts_to_show_as_badges state, contexts do
+    Enum.filter contexts,
+      fn ctx ->
+        (is_nil state.selected_context)
+        or ctx.context.title != state.selected_context.title
+      end
   end
 end
