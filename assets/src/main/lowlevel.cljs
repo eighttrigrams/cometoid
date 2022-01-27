c01376eb7765d6be54ad9ff5702cb7e739c3cdf6
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2022-01-27 21:46:06 +0000

    Extract namespace

--- assets/src/main/lowlevel.cljs
+++ assets/src/main/lowlevel.cljs
@@ -1,4 +1,5 @@
-(ns lowlevel)
+(ns lowlevel
+  (:require [lowlevel-helpers :as h]))
 
 (def word-stop-pattern "[,;.-_\\n]")
 
@@ -18,39 +19,15 @@
                       selection-start)
    :value value})
 
-(defn starts-with-pattern? [s pattern]
-  (not (nil? (re-find (re-pattern (str "^" pattern)) s))))
-
-(defn index-of-substr-or-end [s pattern]
-  (loop [rst s
-         i 0]
-    (if (= i (count s))
-      i
-      (if-not (starts-with-pattern? rst pattern)
-        (recur (apply str (rest rst)) (inc i))
-        i))))
-
-(defn reverse-state [{value           :value
-                      selection-start :selection-start}]
-  {:value           (apply str (reverse value))
-   :selection-start (- (count value) selection-start)})
-
-(defn leftwards [fun]
-  (fn [state]
-    (-> state reverse-state fun reverse-state)))
-
-(defn calc-rest [{value :value selection-start :selection-start}]
-  (subs value selection-start (count value)))
-
 (defn word-part-right [{value :value selection-start :selection-start :as state}]
-  (let [rest            (calc-rest state)
+  (let [rest            (h/calc-rest state)
         selection-start (+ selection-start
-                           (cond (starts-with-pattern? rest word-stop-pattern)
+                           (cond (h/starts-with-pattern? rest word-stop-pattern)
                                  1
-                                 (starts-with-pattern? rest "[ ]")
-                                 (index-of-substr-or-end rest "[^ ]")
+                                 (h/starts-with-pattern? rest "[ ]")
+                                 (h/index-of-substr-or-end rest "[^ ]")
                                  :else
-                                 (index-of-substr-or-end rest word-stop-pattern-incl-whitespace)))]
+                                 (h/index-of-substr-or-end rest word-stop-pattern-incl-whitespace)))]
     {:value value
      :selection-start selection-start}))
 
@@ -67,34 +44,34 @@
 
 (def delete-word-part-right (delete-right word-part-right))
 
-(def delete-word-part-left (leftwards delete-word-part-right))
+(def delete-word-part-left (h/leftwards delete-word-part-right))
 
-(def word-part-left (leftwards word-part-right))
+(def word-part-left (h/leftwards word-part-right))
 
 (defn sentence-part-right [{value :value selection-start :selection-start :as state}]
-  (let [rest (calc-rest state)
+  (let [rest (h/calc-rest state)
         selection-start (+ selection-start
-                           (if (starts-with-pattern? rest sentence-stop-pattern)
+                           (if (h/starts-with-pattern? rest sentence-stop-pattern)
                              1
-                             (index-of-substr-or-end
+                             (h/index-of-substr-or-end
                               rest
                               sentence-stop-pattern)))]
     {:value value
      :selection-start selection-start}))
 
-(def sentence-part-left (leftwards sentence-part-right))
+(def sentence-part-left (h/leftwards sentence-part-right))
 
 (def delete-sentence-part-right (delete-right sentence-part-right))
 
-(def delete-sentence-part-left (leftwards delete-sentence-part-right))
+(def delete-sentence-part-left (h/leftwards delete-sentence-part-right))
 
 (defn newline-after-current [{value :value selection-start :selection-start :as state}]
   (if (= selection-start (count value))
     {:value (str value "\n")
      :selection-start (inc selection-start)}
-    (let [rest (calc-rest state)
-          i    (+ selection-start (index-of-substr-or-end rest "\\n"))]
+    (let [rest (h/calc-rest state)
+          i    (+ selection-start (h/index-of-substr-or-end rest "\\n"))]
       {:selection-start (inc i)
        :value           (str (subs value 0 i) "\n" (subs value i (count value)))})))
 
-(def newline-before-current (leftwards newline-after-current))
+(def newline-before-current (h/leftwards newline-after-current))
---
+++ assets/src/main/lowlevel_helpers.cljs
@@ -0,0 +1,25 @@
+(ns lowlevel-helpers)
+
+(defn starts-with-pattern? [s pattern]
+  (not (nil? (re-find (re-pattern (str "^" pattern)) s))))
+
+(defn index-of-substr-or-end [s pattern]
+  (loop [rst s
+         i 0]
+    (if (= i (count s))
+      i
+      (if-not (starts-with-pattern? rst pattern)
+        (recur (apply str (rest rst)) (inc i))
+        i))))
+
+(defn reverse-state [{value           :value
+                      selection-start :selection-start}]
+  {:value           (apply str (reverse value))
+   :selection-start (- (count value) selection-start)})
+
+(defn leftwards [fun]
+  (fn [state]
+    (-> state reverse-state fun reverse-state)))
+
+(defn calc-rest [{value :value selection-start :selection-start}]
+  (subs value selection-start (count value)))
