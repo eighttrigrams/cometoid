acfcb446ba6cb7ba235b7c83d05e3b0134bb9753
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2022-01-30 01:29:01 +0000

    Extract bindings namespace

--- assets/src/main/editor.cljs
+++ assets/src/main/editor.cljs
@@ -1,6 +1,7 @@
 (ns editor
   (:require lowlevel
-            machine))
+            machine
+            bindings))
 
 (defn hey [s] (str s s))
 
@@ -44,53 +45,7 @@
             apply-action-and-track (apply-action-and-track el)]
         (apply-action-and-track (lowlevel/insert clipboard-data) (convert el)))))
 
-(defn get-command [is-pressed?]
-  (cond (is-pressed? "KeyY" #{:ctrl})
-        :restore
-        (is-pressed? "KeyL" #{:ctrl})
-        :caret-left
-        (is-pressed? "KeyJ" #{:ctrl})
-        :caret-right
-        (is-pressed? "KeyL" #{:shift :ctrl})
-        :caret-left-with-selection
-        (is-pressed? "KeyJ" #{:shift :ctrl})
-        :caret-right-with-selection
-        (is-pressed? "KeyL" #{:meta})
-        :word-part-left
-        (is-pressed? "KeyJ" #{:meta})
-        :word-part-right
-        (is-pressed? "KeyL" #{:shift :meta})
-        :word-part-right-with-selection
-        (is-pressed? "KeyJ" #{:shift :meta})
-        :word-part-left-with-selection
-        (is-pressed? "KeyL" #{:alt})
-        :sentence-right
-        (is-pressed? "KeyJ" #{:alt})
-        :sentence-left
-        (is-pressed? "KeyL" #{:shift :alt})
-        :sentence-right-with-selection
-        (is-pressed? "KeyJ" #{:shift :alt})
-        :sentence-left-with-selection
-        (is-pressed? "Backspace" #{:shift})
-        :shift-backspace
-        (is-pressed? "Backspace" #{:meta})
-        :meta-backspace
-        (is-pressed? "Backspace" #{:shift :meta})
-        :shift-meta-backspace
-        (is-pressed? "Backspace" #{:alt})
-        :alt-backspace
-        (is-pressed? "Backspace" #{:shift :alt})
-        :shift-alt-backspace
-        (is-pressed? "Enter" #{:shift})
-        :shift-enter
-        (is-pressed? "Enter" #{:alt})
-        :alt-enter
-        (is-pressed? "KeyV" #{:ctrl})
-        :keyv-ctrl
-        (is-pressed? "KeyX" #{:ctrl})
-        :keyx-ctrl
-        (is-pressed? "KeyC" #{:ctrl})
-        :keyc-ctrl))
+
 
 (defn clean [{selection-start :selection-start selection-end :selection-end value :value}]
   {:value value :selection-start selection-start :selection-end selection-end})
@@ -99,7 +54,7 @@
   (fn [e]
     (set-modifiers! e true modifiers)
     (let [is-pressed? (is-pressed? e @modifiers)
-          key         (get-command is-pressed?)
+          key         (bindings/get-command is-pressed?)
           state       (convert el direction history)
           {dir :direction :as new-state}      (machine/handle-key key state)]
 
