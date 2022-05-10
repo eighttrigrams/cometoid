23d74e667205f1cb95ccb7916e2d38144517ffe5
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2022-05-10 23:04:09 +0100

    Reprioritize issue on select

--- lib/cometoid_web/issue_live/index.ex
+++ lib/cometoid_web/issue_live/index.ex
@@ -396,8 +396,10 @@ defmodule CometoidWeb.IssueLive.Index do
   end
 
   def handle_event "select_issue", %{ "target" => id }, socket do
+    IO.inspect id
     selected_issue = Tracker.get_issue! id
     socket
+    |> push_event(:issue_reprioritized, %{ id: id})
     |> assign(:selected_issue, selected_issue)
     |> assign(:issue_search_active, false)
     |> return_noreply
