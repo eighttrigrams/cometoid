4cc0fbd4662727c1be5a3289843d158163b23889
Dan <dan@eighttrigrams.net>
2022-07-08 20:57:46 +0100

    Put state into :state property of assigns

--- lib/cometoid_web/issue_live/wrap_handle.ex
+++ lib/cometoid_web/issue_live/wrap_handle.ex
@@ -6,11 +6,22 @@ defmodule CometoidWeb.IssueLive.WrapHandle do
     end
   end
 
+  defmacro def({:handle_params, _, _} = name_and_args, do: code) do
+    quote do
+      def unquote(name_and_args) do
+        socket = unquote(code)
+        socket
+        |> assign(:action, :handle_params)
+        |> return_noreply
+      end
+    end
+  end
+
   defmacro def({:handle_info, _, [r|_]} = name_and_args, do: code) do
     [name|_] = Tuple.to_list r
     quote do
       def unquote(name_and_args) do
-        {:noreply, socket} = unquote(code)
+        socket = unquote(code)
         socket
         |> assign(:action, unquote(name))
         |> return_noreply
@@ -21,7 +32,7 @@ defmodule CometoidWeb.IssueLive.WrapHandle do
   defmacro def({:handle_event, _, [name|_]} = name_and_args, do: code) do
     quote do
       def unquote(name_and_args) do
-        {:noreply, socket} = unquote(code)
+        socket = unquote(code)
         socket
         |> assign(:action, unquote(name))
         |> return_noreply
