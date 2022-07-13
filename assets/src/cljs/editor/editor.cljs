945036596d4935628c2d98b9e5a857b4d4234926
Dan <dan@eighttrigrams.net>
2022-07-13 20:32:57 +0100

    Use latest editor

--- assets/src/cljs/editor/editor.cljs
+++ assets/src/cljs/editor/editor.cljs
@@ -1,17 +1,5 @@
 (ns editor.editor
-  (:require [net.eighttrigrams.cljs-text-editor.editor :as editor]
-            [net.eighttrigrams.cljs-text-editor.machine :as machine]
-            [net.eighttrigrams.cljs-text-editor.bindings :as bindings]
-            [net.eighttrigrams.cljs-text-editor.bindings-resolver :as bindings-resolver]
-            [net.eighttrigrams.cljs-text-editor.time-machine :as time-machine]))
+  (:require [net.eighttrigrams.cljs-text-editor.editor :as editor]))
 
 (defn ^:export new [el]
-  (let [modifiers (atom #{})
-        position-in-line (atom nil)
-        resolve-bindings (bindings-resolver/build bindings/commands)
-        transform-state (-> (machine/build) time-machine/build resolve-bindings)]
-    (.addEventListener el "paste" (editor/paste el modifiers transform-state position-in-line))
-    (.addEventListener el "keydown" (editor/keydown el modifiers transform-state position-in-line))
-    (.addEventListener el "keyup" (editor/keyup el modifiers))
-    (.addEventListener el "mouseleave" (editor/mouseleave el modifiers))
-    (.addEventListener el "click" (editor/click el position-in-line))))
+  (editor/create el))
