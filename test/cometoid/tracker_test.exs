128617c9b08e9826c4c494e5f90612a796aae67e
Dan <dan@eighttrigrams.net>
2022-08-04 20:00:52 +0100

    Use describe

--- test/cometoid/tracker_test.exs
+++ test/cometoid/tracker_test.exs
@@ -33,76 +33,75 @@ defmodule Cometoid.TrackerTest do
     assert c2.issues == []
   end
 
-  ## SEARCH
+  describe "search" do
 
-  #  TODO consider short title in search
+    test "prefix search issues" do
+      {:ok, c1} = Tracker.create_context %{ "title" => "context1", "view" => "view1" }    
+      Tracker.create_issue "abc", "short title 1", [c1]
+      Tracker.create_issue "abd", "short title 2", [c1]
+      Tracker.create_issue "acd", "short title 2", [c1]
 
-  test "prefix search issues" do
-    {:ok, c1} = Tracker.create_context %{ "title" => "context1", "view" => "view1" }    
-    Tracker.create_issue "abc", "short title 1", [c1]
-    Tracker.create_issue "abd", "short title 2", [c1]
-    Tracker.create_issue "acd", "short title 2", [c1]
-
-    query = %{
-      selected_context: nil,
-      list_issues_done_instead_open: false,
-      sort_issues_alphabetically: false,
-      selected_view: "view1",
-      selected_issue: nil,
-      search: %{
-        q: "ab",
-        show_all_issues: false
+      query = %{
+        selected_context: nil,
+        list_issues_done_instead_open: false,
+        sort_issues_alphabetically: false,
+        selected_view: "view1",
+        selected_issue: nil,
+        search: %{
+          q: "ab",
+          show_all_issues: false
+        }
       }
-    }
 
-    issues = Tracker.list_issues query
-    assert (MapSet.new ["abc", "abd"]) 
-      == MapSet.new Enum.map issues, &(&1.title)
-  end 
+      issues = Tracker.list_issues query
+      assert (MapSet.new ["abc", "abd"]) 
+        == MapSet.new Enum.map issues, &(&1.title)
+    end 
 
-  test "prefix search issues on short_title, too" do
-    {:ok, c1} = Tracker.create_context %{ "title" => "context1", "view" => "view1" }    
-    Tracker.create_issue "abc", "short title 1", [c1]
-    Tracker.create_issue "aaa", "abd", [c1]
-    Tracker.create_issue "acd", "ace", [c1]
+    test "prefix search issues on short_title, too" do
+      {:ok, c1} = Tracker.create_context %{ "title" => "context1", "view" => "view1" }    
+      Tracker.create_issue "abc", "short title 1", [c1]
+      Tracker.create_issue "aaa", "abd", [c1]
+      Tracker.create_issue "acd", "ace", [c1]
 
-    query = %{
-      selected_context: nil,
-      list_issues_done_instead_open: false,
-      sort_issues_alphabetically: false,
-      selected_view: "view1",
-      selected_issue: nil,
-      search: %{
-        q: "ab",
-        show_all_issues: false
+      query = %{
+        selected_context: nil,
+        list_issues_done_instead_open: false,
+        sort_issues_alphabetically: false,
+        selected_view: "view1",
+        selected_issue: nil,
+        search: %{
+          q: "ab",
+          show_all_issues: false
+        }
       }
-    }
 
-    issues = Tracker.list_issues query
-    assert (MapSet.new ["abc", "aaa"]) 
-      == MapSet.new Enum.map issues, &(&1.title)
-  end 
+      issues = Tracker.list_issues query
+      assert (MapSet.new ["abc", "aaa"]) 
+        == MapSet.new Enum.map issues, &(&1.title)
+    end 
 
-  test "multiple search terms (AND search)" do
-    {:ok, c1} = Tracker.create_context %{ "title" => "context1", "view" => "view1" }    
-    Tracker.create_issue "abc aaa", "", [c1]
-    Tracker.create_issue "aaa", "", [c1]
-    Tracker.create_issue "ccc", "", [c1]
+    test "multiple search terms (AND search)" do
+      {:ok, c1} = Tracker.create_context %{ "title" => "context1", "view" => "view1" }    
+      Tracker.create_issue "abc aaa", "", [c1]
+      Tracker.create_issue "aaa", "", [c1]
+      Tracker.create_issue "ccc", "", [c1]
 
-    query = %{
-      selected_context: nil,
-      list_issues_done_instead_open: false,
-      sort_issues_alphabetically: false,
-      selected_view: "view1",
-      selected_issue: nil,
-      search: %{
-        q: "ab aa",
-        show_all_issues: false
+      query = %{
+        selected_context: nil,
+        list_issues_done_instead_open: false,
+        sort_issues_alphabetically: false,
+        selected_view: "view1",
+        selected_issue: nil,
+        search: %{
+          q: "ab aa",
+          show_all_issues: false
+        }
       }
-    }
 
-    issues = Tracker.list_issues query
-    assert (MapSet.new ["abc aaa"]) 
-      == MapSet.new Enum.map issues, &(&1.title)
+      issues = Tracker.list_issues query
+      assert (MapSet.new ["abc aaa"]) 
+        == MapSet.new Enum.map issues, &(&1.title)
+    end
   end
 end
