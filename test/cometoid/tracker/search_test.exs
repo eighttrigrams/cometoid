9ea4b4ccf94510942f2c2a1f2d255b02ef4ae589
Dan <dan@eighttrigrams.net>
2022-08-05 19:40:27 +0100

    Prepare filter secondary contexts test

--- test/cometoid/tracker/search_test.exs
+++ test/cometoid/tracker/search_test.exs
@@ -3,8 +3,26 @@ defmodule Cometoid.Tracker.SearchTest do
 
   alias Cometoid.Repo.Tracker
   alias Cometoid.Repo.Tracker.Search
+  alias CometoidWeb.IssueLive.IssuesMachine # TODO <- should be in Cometoid, not CometoidWeb
 
-  describe "search" do
+  def titles_set_from issues do # TODO use throughout tests
+    MapSet.new Enum.map issues, &(&1.title)
+  end
+
+  test "filter for secondary contexts" do
+    {:ok, c1} = Tracker.create_context %{ "title" => "context1", "view" => "view1" }    
+    {:ok, c2} = Tracker.create_context %{ "title" => "context2", "view" => "view1" }    
+    Tracker.create_issue "1", "short title 1", [c1]
+    Tracker.create_issue "2", "short title 2", [c1, c2]
+
+    state = (IssuesMachine.State.new "view1")
+      |> put_in([:selected_context], c1)
+      |> put_in([:selected_secondary_contexts], [c2.id])
+    issues = titles_set_from Search.list_issues state
+    assert (MapSet.new ["2"]) == issues
+  end
+
+  describe "fulltext" do
 
     test "prefix search issues" do
       {:ok, c1} = Tracker.create_context %{ "title" => "context1", "view" => "view1" }    
@@ -12,6 +30,7 @@ defmodule Cometoid.Tracker.SearchTest do
       Tracker.create_issue "abd", "short title 2", [c1]
       Tracker.create_issue "acd", "short title 2", [c1]
 
+      # TODO use IssuesMachine.State
       query = %{
         selected_context: nil,
         list_issues_done_instead_open: false,
